bf85cc79b9d2b3cae0caab46974d8fadc2b19e2b
Ben Rooney <ben@benrooney.com>
2018-07-21 17:57:10 +0100

    Think have sorted out the nesting. Adding functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,8 @@ class CommentsController < ApplicationController
   # GET /comments
   # GET /comments.json
   def index
-    @comments = Comment.all
+    @comments = Comment.where("homework_id = ?", @homework.id)
+
   end
 
   # GET /comments/1
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,6 +10,7 @@ class StudentsController < ApplicationController
   # GET /students/1
   # GET /students/1.json
   def show
+    @homeworks = @student.class_group.homeworks
   end
 
   # GET /students/new
