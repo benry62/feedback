92c7f2ae844f760668884b5a700a49644f38a383
Ben Rooney <ben@benrooney.com>
2018-07-27 22:37:00 +0100

    Export PDF all working

--- app/pdfs/export_pdf.rb
+++ app/pdfs/export_pdf.rb
@@ -4,13 +4,18 @@ class ExportPdf < Prawn::Document
     super(page_size: "A4")
     define_grid(:columns => 6, :rows => 21  , :gutter => 5)
     @logo = "#{Rails.root}/app/assets/images/swan_logo.png"
+    font_families.update("zapfdingbats" => {:normal => "#{Rails.root}/app/assets/fonts/zapfdingbats.ttf"} )
+
+
     @homework = homework
     @comments = @homework.comments
+    @presentation_items = presentation_items
     @comments.each do |comment|
       form_header(comment)
       www (comment)
       feedback (comment)
-      presentation(comment,presentation_items)
+      presentation(comment)
+      dirt(comment)
       spelling (comment)
       start_new_page
     end
@@ -76,42 +81,118 @@ class ExportPdf < Prawn::Document
     grid([6, 3], [10, 5]).bounding_box do
       pad(12){
         indent (10) do
-          text "\nWhat steps I will take", size: 14, :style => :bold
+          text "\nSteps I will take", size: 14, :style => :bold
         end
       }
     end
   end # feedback
 
-  def build_pres_items(presentation_items)
-    items_array = []
-    categories = []
-    headings = []
-    ids = []
-    presentation_items.each do |pi|
-      categories << pi.category
-      headings << pi.heading
-      ids << pi.id
+  def build_pres_items_hash
+    categories  = {}
+    headings    = {}
+    ids         = {}
+    i           = 0
+    @presentation_items.each do |pi|
+      categories[i] = pi.category
+      headings[i]   = pi.heading
+      ids[i]        = pi.id
+      i += 1
     end
+    return ids, headings, categories
+  end
 
+
+  def checked (id_array, id)
+    id_array.include?(id) ? "3 " : "7 "
+  end
+
+  def pres_items_ids (comment)
+    # builds array of presentation_item_id for a given comment
+    id_array = []
+    comment.presentations.each do |p|
+      id_array << p.presentation_item_id
+    end
+    id_array
   end
 
 
 
-  def presentation(comment,presentation_items)
+  def presentation(comment)
     grid([11, 0], [17, 1]).bounding_box do
       stroke_bounds
+      cat = ""
+      ids, headings, categories = build_pres_items_hash
+      id_array = pres_items_ids (comment)
+
       pad(12){
         indent (10) do
           text "Presentation", size: 16, :style => :bold
-          presentation_items.each do |pres|
-            text pres.heading
-            text pres.category
-
+          text headings[0], size: 14, :style => :bold
+          indent (20) do
+            first_cat = checked(id_array, ids[0]) + categories[0]
+            font("zapfdingbats") do
+              text checked(id_array, ids[0])
+            end
+            move_up 11
+            indent (12) do
+              text categories[0]
+            end
           end
+          for i in 1..ids.length-1 do
+            #step through the arrays.
+            #put the first title. Then check to see if the next title is
+            #the same. If it is then put the first category
+            #check the id. If the id is in the comment.presentation.presentation_item_ids
+            #then write a tick. If not write a cross.
+
+            #Then move along to the next item.
+            if headings[i] == headings[i-1]
+              indent (20) do
+                font("zapfdingbats") do
+                  text checked(id_array, ids[i])
+                end
+                move_up 11
+                indent (12) do
+                  text categories[i]
+                end
+#                text checked(id_array, ids[i]) + categories[i]
+              end # intent
+              i += 1
+            else
+              text headings[i], size: 14, :style => :bold
+              indent (20) do
+                font("zapfdingbats") do
+                  text checked(id_array, ids[i])
+                end
+                move_up 11
+                indent (12) do
+                  text categories[i]
+                end
+              end #do
+            end #if
+          end #for
+        end # indent
+      } #pad
+    end #grid
+  end #presentation
+
+  def dirt(comment)
+    grid([11, 2], [17, 5]).bounding_box do
+      stroke_bounds
+      pad(12){
+        indent (10) do
+          text "DIRT", size: 16, :style => :bold
+          text comment.dirt, size: 14
         end
       }
     end
-  end #presentation
+
+
+
+
+  end
+
+
 
   def spelling(comment)
     grid([18, 0], [20, 5]).bounding_box do
