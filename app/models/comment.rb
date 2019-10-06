64aa45cda086b4072e99b3266f717abeca876117
Ben Rooney <ben@benrooney.com>
2019-10-06 13:23:13 +0100

    Tweaks

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -20,12 +20,15 @@ class Comment < ApplicationRecord
 
   def calculate_class (total_score)
     if result.to_f/total_score.to_f > 0.85
+      self.merit = TRUE
       css_class = "merit"
     elsif result.to_f/total_score.to_f < 0.49
+      self.resubmit = TRUE
       css_class = "resubmit"
     else
       css_class = ''
     end
+    self.save
     css_class
   end
 
