02428d7c686e47269ff514132e99deec2b0d913f
Ben Rooney <ben@benrooney.com>
2019-10-26 15:28:07 +0100

    Add back in the resubmit flag.

--- app/models/class_group.rb
+++ app/models/class_group.rb
@@ -9,7 +9,7 @@ class ClassGroup < ApplicationRecord
     comments = Comment.where("homework_id = ? ", homework.id)
     scores = Hash.new
     comments.each do |comment|
-      attributes = {:result => comment.result, :is_merit => comment.merit, :is_resubmit => comment.resubmit, :not_submitted => comment.not_submitted}
+      attributes = {:result => comment.result, :is_merit => comment.merit, :resubmit => comment.resubmit, :not_submitted => comment.not_submitted}
       scores.store(comment.student_id, attributes)
     end
     scores
